bd2efd93732d8a864f74f86884aae46fd649daaf
ayham <me@ayham.xyz>
2023-08-26 10:12:14 +0300

    improve development workflow

--- flake.nix
+++ flake.nix
@@ -10,33 +10,30 @@
         flake-utils.lib.eachDefaultSystem (system : let 
             pkgs = import nixpkgs { inherit system; };
         in {
-            packages = {
-                default = pkgs.stdenv.mkDerivation {
+        packages = {
+            default = pkgs.stdenv.mkDerivation {
                     name = "minide";
                     src = ./.;
 
                     nativeBuildInputs = with pkgs; [
                         clang
                         cmake
+                        ninja
                     ];
 
                     buildInputs = with pkgs; [
                         glfw
                         glew
                     ];
-
                     shellHook = ''
                         export PS1="minide> "
                         rm -rf .build/
                         mkdir .build/
-                        cd .build/
-                        echo "cmake -DCMAKE_BUILD_TYPE=Debug .. && make && ./minide" > debug.sh
-                        chmod +x debug.sh
-
-                        echo "cmake -DCMAKE_BUILD_TYPE=Release .. && make && ./minide" > release.sh
-                        chmod +x release.sh
+                        alias debug="cmake -B.build -DCMAKE_BUILD_TYPE=Debug -GNinja && ninja -C .build && ./.build/minide"
+                        alias release="cmake -B.build -DCMAKE_BUILD_TYPE=Release -GNinja && ninja -C .build && ./.build/minide"
+                        alias ide="nix run github:ayham-1/ide"
                     '';
-                };
             };
-        });
+        };
+    });
 }
